c3d2a54d7912848904e046024e4ee885a88e8efb
rgarciapedroza <rosmarygp11@gmail.com>
2024-04-24 13:14:05 +0000

    error fixed at sala.c

--- trabajo/src/sala.c
+++ trabajo/src/sala.c
@@ -71,27 +71,7 @@ int asientos_libres() {
 
 int asientos_ocupados() {
     return capacidad_sala() - asientos_libres();
-}#ifndef SALA_H
-#define SALA_H
-#include <stddef.h>
-
-void imprimir();
-int crea_sala (int capacidad);
-int reserva_asiento (int id_persona);
-int libera_asiento (int id_asiento);
-int estado_asiento(int id_asiento);
-
-int asientos_libres ();
-int asientos_ocupados ();
-int capacidad_sala ();
-int elimina_sala();
-
-int guarda_estado_sala(char* ruta_fichero);
-int recupera_estado_sala(char* ruta_fichero);
-int guarda_estadoparcial_sala( char* ruta_fichero, size_t num_asientos, int* id_asientos);
-int recupera_estadoparcial_sala( char* ruta_fichero, size_t num_asientos, int* id_asientos);
-
-#endif
+}
 
 int capacidad_sala() {
     return asientos;
